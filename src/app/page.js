283852cd4f1fac89eb83f2e33090c7b7b7da067c
gswirrl <guyjohnhilton@gmail.com>
2024-07-18 08:28:49 +0100

    static asset test

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Head from 'next/head'
 
 export default function Home() {
   return (
     <main>
-
+ <Image
+      src="/reteti.png"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
       <h1 className={styles.h1}>Hello World</h1>
       <h1 className={`${styles.h1} ${styles.blue}`}>Hello World</h1>
       <h1 className={`${styles.h1} ${styles.lightblue}`}>Hello World</h1>
