c1c91172f4339a4c24262d5a858824e00407c431
gswirrl <guyjohnhilton@gmail.com>
2024-08-13 00:25:48 +0100

    turn iff

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,10 @@
-'use client'
-
-import React, { useEffect, useState } from 'react'
-import { CounterAPI } from 'counterapi'
-import { useCookies } from 'react-cookie'
-import { Thanks } from '@/components/Thanks'
+import React from 'react'
+import { Finished } from '@/components/Finished'
 import { Masthead } from '@/components/Masthead'
 import { Logo } from '@/components/Logo'
-import { Countdown } from '@/components/Countdown'
-import { Milk } from '@/components/Milk'
 
-const COUNTER_NAMESPACE = 'tpxelephant24'
-const COUNTER_NAME = 'milk24'
-const COOKIE_NAME = 'wed2024'
-
-const counter = new CounterAPI()
 
 const Home = () => {
-	const [count, setCount] = useState(null)
-	const [cookies, setCookie] = useCookies([COOKIE_NAME])
-	const [wasCounted, setWasCounted] = useState()
-
-	useEffect(() => {
-		if (!cookies[COOKIE_NAME]) {
-			console.log('counter')
-			counter.up(COUNTER_NAMESPACE, COUNTER_NAME).then(res => {
-				setCount(res.Count)
-				setCookie(COOKIE_NAME, true)
-				setWasCounted(true)
-			})
-		} else {
-			counter.get(COUNTER_NAMESPACE, COUNTER_NAME).then(res => {
-				setCount(res.Count)
-				setWasCounted(false)
-			})
-		}
-	}, [])
 
 	return (
 		<main>
@@ -43,9 +13,7 @@ const Home = () => {
 			</noscript>
 			<Masthead />
 			<Logo />
-			{wasCounted && <Thanks />}
-			<Countdown jump={true} />
-			<Milk count={count} />
+			<Finished />
 		</main>
 	)
 }
---
+++ src/components/Finished/Finsihed.js
@@ -0,0 +1,8 @@
+import styles from './Finished.module.css'
+
+export const Finished = () => (
+	<div className={styles.finished}>
+		<h1>Thank you!</h1>
+		<h2>World Elephant Day has now finished.</h2>
+	</div>
+)
